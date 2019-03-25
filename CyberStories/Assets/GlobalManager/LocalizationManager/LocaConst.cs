8d079df975c4b2dddd13dcf82cabfe78c466b23d
AArzop <37124191+AArzop@users.noreply.github.com>
2019-03-25 15:20:23 +0100

    Add Loca

--- CyberStories/Assets/GlobalManager/LocalizationManager/LocaConst.cs
+++ CyberStories/Assets/GlobalManager/LocalizationManager/LocaConst.cs
@@ -1,4 +1,6 @@
 public static class LocaConst
 {
-	public const uint Loca_Menu = 1;
+	public const uint Loca_New = 1;
+	public const uint Loca_MailArchive = 2;
+	public const uint Loca_MailDelete = 3;
 }
