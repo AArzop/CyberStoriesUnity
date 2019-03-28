0c0ff3f452e493cb441e0b97851cfc4d97514727
AArzop <37124191+AArzop@users.noreply.github.com>
2019-03-28 14:09:16 +0100

    Begin webApplication

---
+++ CyberStories/Assets/ComputerScreen/Application/WebApplication/Script/BaseWebSite.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseWebSite : MonoBehaviour
+{
+    public string Name;
+    public Sprite Icon;
+
+    public string Url { get; set; }
+}
---
+++ CyberStories/Assets/ComputerScreen/Application/WebApplication/Script/WebApplication.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebApplication : BaseApplication
+{
+    public List<BaseWebSite> webSites;
+    public List<BaseWebSite> favoriteWebSites;
+
+    public List<FavoriteButton> favoriteButtons;
+
+    private Dictionary<string, BaseWebSite> urlToWebSite;
+
+    private void Awake()
+    {
+        foreach (var Site in webSites)
+            urlToWebSite.Add(Site.Url, Site);
+
+        for (int i = 0; i < favoriteButtons.Count; i++)
+        {
+            if (i < favoriteWebSites.Count)
+                favoriteButtons[i].ApplyWebSite(favoriteWebSites[i]);
+            else
+                favoriteButtons[i].ApplyWebSite(null);
+        }
+    }
+
+    public override void ResetApplication()
+    {
+        
+    }
+
+    public void Redirect(BaseWebSite webSite)
+    {
+
+    }
+}
