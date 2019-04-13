8611db0a5e7829e63a0fcbc19553c38e9c3e8deb
AArzop <37124191+AArzop@users.noreply.github.com>
2019-04-13 02:08:28 +0200

    Create base goal on basket mini game

---
+++ CyberStories/Assets/Gameplay/Phishing/BaseBasketPlay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseBasketPlay : MonoBehaviour
+{
+    private BasketGame game;
+    public BoxCollider goal;
+
+    public float multiplicator = 1f;
+
+    protected void Awake()
+    {
+        game = FindObjectOfType<BasketGame>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            var a = GameObject.Find(other.name);
+            BasketGameBall ball = a.GetComponent<BasketGameBall>();
+            if (ball != null && !ball.isGrabbed)
+                game.endBasketGame();
+        }
+    }
+
+    public Vector3 GetGoalPosition()
+    {
+        return goal.transform.position;
+    }
+
+    protected abstract void Update();
+}
