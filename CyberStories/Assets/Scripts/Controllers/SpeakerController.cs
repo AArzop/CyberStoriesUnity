b71d9f11a487b008693c4836916427709b55aa7b
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-04-15 14:32:03 +0200

    Office: Speaker: Add a switch on the speaker & Create a script that can switch on/off the audio

---
+++ CyberStories/Assets/Scripts/Controllers/SpeakerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace CyberStories.Controllers
+{
+    public class SpeakerController : MonoBehaviour
+    {
+        public AudioSource _audioSource;
+
+        public void SwitchOnOffAudio()
+        {
+            if (_audioSource.isPlaying)
+                _audioSource.Stop();
+            else
+                _audioSource.Play();
+        }
+    }
+}
