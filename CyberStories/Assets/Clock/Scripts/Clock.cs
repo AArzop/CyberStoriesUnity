7aafe5d055afbc4f7daa8d60dd561587d32ad9f2
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-04-24 16:03:57 +0200

    Clock: Display current time if we set isLocalTime to true

--- CyberStories/Assets/Clock/Scripts/Clock.cs
+++ CyberStories/Assets/Clock/Scripts/Clock.cs
@@ -9,7 +9,8 @@ public class Clock : MonoBehaviour {
     // set start time 00:00
     public int minutes = 0;
     public int hour = 0;
-    
+    // If true, take hour, minutes and seconds will be updated according to DateTime.Now
+    public bool isLocalTime;
     // time speed factor
     public float clockSpeed = 1.0f;     // 1.0f = realtime, < 1.0f = slower, > 1.0f = faster
 
@@ -30,6 +31,14 @@ public class Clock : MonoBehaviour {
 
         msecs = 0.0f;
         seconds = 0;
+
+        if (isLocalTime)
+        {
+            DateTime currentTime = DateTime.Now;
+            hour = currentTime.Hour;
+            minutes = currentTime.Minute;
+            seconds = currentTime.Second;
+        }
     }
 
     void Update() 
