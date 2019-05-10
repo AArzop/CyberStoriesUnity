36d684b223db8e8b85b0beca19c90f13b758da1b
Lieema <axel.vigny@epita.fr>
2019-05-10 15:49:24 +0200

    start of dialogue system

---
+++ CyberStories/Assets/Scripts/Dialogue/DialogueSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CyberStories.DBO;
+using System;
+
+public class DialogueSystem : MonoBehaviour
+{
+    private  DialogueLine actualLine;
+
+    public void StartDialogue(DialogueLine firstLine)
+    {
+        actualLine = firstLine;
+
+        Debug.Log("start dialogue of " + actualLine.name);
+
+        nextLine();
+    }
+
+    private void nextLine()
+    {
+        if (actualLine.answers.Length == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        Debug.Log(actualLine.name + ": " + actualLine.textLine);
+    }
+
+    private void EndDialogue()
+    {
+        Debug.Log("End of dialogue");
+    }
+}
---
+++ CyberStories/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,19 @@
+﻿using CyberStories.DBO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class DialogueTrigger : MonoBehaviour
+{
+    public DialogueLine firstLine;
+
+    private void HandHoverUpdate(Hand hand)
+    {
+        GrabTypes startingGrabType = hand.GetGrabStarting();
+
+        if (startingGrabType != GrabTypes.None)
+            FindObjectOfType<DialogueSystem>().StartDialogue(firstLine);
+    }
+}
