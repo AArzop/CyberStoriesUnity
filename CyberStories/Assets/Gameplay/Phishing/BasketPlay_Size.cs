76874fe82e41593d5f9925058c0514b43540a925
AArzop <37124191+AArzop@users.noreply.github.com>
2019-05-23 14:41:07 +0200

    Re integrated old prefab

---
+++ CyberStories/Assets/Gameplay/Phishing/BasketPlay_Size.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketPlay_Size : BaseBasketPlay
+{
+    public float minSize;
+    public float maxSize;
+    public float speed;
+
+    public bool firstIncrease = true;
+
+    private float sign;
+
+    private void Start()
+    {
+        sign = firstIncrease ? 1f : -1f;
+    }
+
+    protected override void Update()
+    {
+        float size = transform.localScale.x; // x, y, z are equals
+
+        if (size > maxSize || size < minSize)
+            sign = -sign;
+
+        size += sign * speed * Time.deltaTime;
+
+        transform.localScale = new Vector3(size, size, size);
+    }
+}
