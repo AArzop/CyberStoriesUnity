5ad80832b5c0aceba69b7640e97f50f6986e5dc2
AArzop <37124191+AArzop@users.noreply.github.com>
2019-05-23 14:45:26 +0200

    Re use old ball

---
+++ CyberStories/Assets/Gameplay/Phishing/BasketGameBall.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+[RequireComponent(typeof(Interactable))]
+public class BasketGameBall : MonoBehaviour
+{
+    public DateTime launchTime { get; set; }
+    public Vector3 launchPosition { get; set; }
+    public bool isGrabbed { get; set; }
+
+    private void Awake()
+    {
+        launchPosition = Vector3.zero;
+        launchTime = DateTime.Now;
+        isGrabbed = false;
+    }
+
+    protected void OnAttachedToHand(Hand hand)
+    {
+        isGrabbed = true;
+    }
+
+    protected virtual void OnDetachedFromHand(Hand hand)
+    {
+        isGrabbed = false;
+        launchPosition = gameObject.transform.position;
+    }
+}
