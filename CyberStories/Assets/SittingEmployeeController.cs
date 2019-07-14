f97c855912b640b17c7b666ec3b893f0183a3684
Laxman Thayalan <laxman.thayalan@epita.fr>
2019-07-14 21:37:33 +0200

    Add a new NPC in the office

---
+++ CyberStories/Assets/SittingEmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SittingEmployeeController : MonoBehaviour
+{
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetFloat("Horizontal", 0);
+        animator.SetFloat("Vertical", -1);
+    }
+}
