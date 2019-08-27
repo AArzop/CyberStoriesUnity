49425cd0ea4695be398c81c2a1ea3b5aed40309f
meric <meric@LAPTOP-OBGF5NE3>
2019-08-27 20:16:24 +0200

    [tests]: added dummy

---
+++ CyberStories/Assets/Tests/Dummy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class Dummy
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void DummySimplePasses()
+        {
+            // Use the Assert class to test conditions
+            Assert.AreEqual(1, 1);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator DummyWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
