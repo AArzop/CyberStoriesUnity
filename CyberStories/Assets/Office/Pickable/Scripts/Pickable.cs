3c8f9e9cb5cc866300078e13093ffba9eb59e474
Emmanuel <meric.emmanuel@gmail.com>
2019-10-09 16:28:50 +0200

    added inventory

--- CyberStories/Assets/Office/Pickable/Scripts/Pickable.cs
+++ CyberStories/Assets/Office/Pickable/Scripts/Pickable.cs
@@ -6,20 +6,32 @@ public class Pickable : MonoBehaviour
 {
     [HideInInspector]
     public Throwable throwable;
+
+    public Inventory inventory;
     
     // Start is called before the first frame update
     protected virtual void Awake()
     {
         throwable = GetComponent<Throwable>();
+
+        if (!inventory)
+        {
+            Debug.LogWarning(gameObject.ToString() + " is a pickable but does not have any inventory set.");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    protected void Update()
     {
         if (throwable.attached && Input.GetKeyDown(KeyCode.Return))
         {
+            GameObject o = gameObject;
+            
             // deactivate itself like it was set in an inventory
-            gameObject.SetActive(false);
+            o.SetActive(false);
+            
+            // add to inventory
+            inventory.AddItem(o);
         }
     }
 }
