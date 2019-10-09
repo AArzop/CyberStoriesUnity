8fe40a67cb76fd9361778dfd588faa97f10e75bb
AArzop <37124191+AArzop@users.noreply.github.com>
2019-10-09 17:13:47 +0200

    Add answerMenu action

---
+++ CyberStories/Assets/Shared/GlobalManager/AnswerMenu.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class AnswerMenu : MonoBehaviour
+{
+    public SteamVR_Action_Boolean action = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("AnswerMenu");
+    public GameObject menu;
+    private bool visible = false;
+    private Player player;
+
+    private GameMasterManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Player.instance;
+        menu.SetActive(false);
+
+        manager = FindObjectOfType<GameMasterManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkInput();
+
+        if (visible)
+        {
+            menu.SetActive(true);
+        }
+        else
+        {
+            menu.SetActive(false);
+        }
+    }
+
+    private void checkInput()
+    {
+        foreach (var hand in player.hands)
+        {
+            if (action.GetStateUp(hand.handType))
+            {
+                visible = !visible;
+                return;
+            }
+        }
+    }
+
+    public void SendMessageToGameMaster(string message)
+    {
+
+    }
+}
